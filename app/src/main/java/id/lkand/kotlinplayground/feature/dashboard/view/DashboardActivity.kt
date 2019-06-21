c753daac15166dc4522cf84d43b6d2962887904c
Andrea <lk.andrea@ymail.com>
2019-06-22 01:37:29 +0700

    Add DataBinding + RxJava + RxKotlin

--- app/src/main/java/id/lkand/kotlinplayground/feature/dashboard/view/DashboardActivity.kt
+++ app/src/main/java/id/lkand/kotlinplayground/feature/dashboard/view/DashboardActivity.kt
@@ -1,33 +1,53 @@
 package id.lkand.kotlinplayground.feature.dashboard.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import id.lkand.kotlinplayground.R
+import id.lkand.kotlinplayground.databinding.ActivityDashboardBinding
 import id.lkand.kotlinplayground.feature.dashboard.viewmodel.DashboardViewModel
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 
-class DashboardActivity : AppCompatActivity() {
+internal class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
     private val viewModel: DashboardViewModel by lazy {
         ViewModelProviders.of(this).get(DashboardViewModel::class.java)
     }
 
+    private val getTrigger =  BehaviorSubject.create<Boolean>()
+    private val postTrigger =  BehaviorSubject.create<Boolean>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        this.setContentView(R.layout.activity_dashboard)
+        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
         this.bindView()
     }
 
     private fun bindView() {
-        this.viewModel.transform()
+        val didLoad = Observable.just(true)
 
-        this.viewModel.dashboardModel.observe(this, Observer { model ->
-            Log.d("DebugUtil", model.toString())
-        })
+        this.viewModel.transform(
+            DashboardViewModel.Input(
+                didLoad,
+                this.getTrigger,
+                this.postTrigger
+            ))
+            .observe(this, Observer {
+                this.binding.viewmodel = this.viewModel
+                this.binding.handler = this
+            })
     }
 
-    // TODO: DataBinding
-    // TODO: Coroutine, Rx
+    fun handleTapGetButton() {
+        this.getTrigger.onNext(true)
+    }
+
+    fun handleTapPostButton() {
+        this.postTrigger.onNext(true)
+    }
+
 }
--- app/src/main/java/id/lkand/kotlinplayground/feature/dashboard/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/id/lkand/kotlinplayground/feature/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,46 +1,55 @@
 package id.lkand.kotlinplayground.feature.dashboard.viewmodel
 
-import android.util.Log
+import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import id.lkand.kotlinplayground.feature.dashboard.api.DashboardTarget
 import id.lkand.kotlinplayground.feature.dashboard.model.DashboardModel
 import id.lkand.kotlinplayground.provider.NetworkProvider
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 
 internal class DashboardViewModel: ViewModel() {
+    val dashboardModel = MutableLiveData<DashboardModel>()
 
-    internal val dashboardModel = MutableLiveData<DashboardModel>()
+    internal data class Input(
+        val didLoad: Observable<Boolean>,
+        val getTrigger: Observable<Boolean>,
+        val postTrigger: Observable<Boolean>
+    )
 
-    internal fun transform() {
-        NetworkProvider.run {
-            request<DashboardTarget>()
-                .getGeneral()
-                .subscribeOn(Schedulers.computation())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(onNext = { response: DashboardModel ->
-                    this@DashboardViewModel.dashboardModel.value = response
-                }, onError = { error ->
-                    Log.e("Retrofit error", error.message)
-                })
-        }
+    @SuppressLint("CheckResult")
+    internal fun transform(input: Input): MutableLiveData<DashboardModel> {
+        Observable.merge(input.didLoad, input.getTrigger)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy {
+                NetworkProvider.request<DashboardTarget>()
+                    .getGeneral()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy { response: DashboardModel ->
+                        this@DashboardViewModel.dashboardModel.value = response
+                    }
+            }
 
-        NetworkProvider.run {
-            request<DashboardTarget>()
-                .postGeneral()
-                .subscribeOn(Schedulers.computation())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(onNext = { response ->
-                    // Stub
-                }, onError = { error ->
-                    Log.e("Retrofit error", error.message)
-                })
-        }
+        input.postTrigger
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy {
+                NetworkProvider.request<DashboardTarget>()
+                    .postGeneral()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy { response: DashboardModel ->
+                        this@DashboardViewModel.dashboardModel.value = response
+                    }
+            }
+
+        return this.dashboardModel
     }
-
-    // TODO: Modeling
 
 }
 
