e73c7d957cbc96485b1e9172f4295b91f5735647
Andrea <lk.andrea@ymail.com>
2019-07-24 11:50:11 +0700

    Add bottom navigation

--- app/src/main/java/id/lkand/kotlinplayground/activity/main/MainActivity.kt
+++ app/src/main/java/id/lkand/kotlinplayground/activity/main/MainActivity.kt
@@ -2,12 +2,40 @@ package id.lkand.kotlinplayground.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.lkand.kotlinplayground.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
+
+        this.setBottomBar()
+    }
+
+    private fun setBottomBar() {
+        with(findViewById<BottomNavigationView>(R.id.main_bottom_navigation)) {
+            setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.action_first -> {
+                        Log.d("Timber", "First")
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    R.id.action_second -> {
+                        Log.d("Timber", "Second")
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    R.id.action_third -> {
+                        Log.d("Timber", "Third")
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                }
+
+                false
+            }
+        }
+
     }
 }
